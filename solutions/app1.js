ffb561a5431bd0bc643d2416a00a5c20e177131d
Kocsis Gábor <ifjkocsis.gabor@gmail.com>
2021-11-22 17:36:59 +0100

    ready

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,12 @@
 
 
+const setCookie = (string ='') => document.cookie =
+`token=${string}; expires=${new Date((new Date).getTime() + 1000 * 60 * 15) }`
+    
 
 
 
 
-// export default setCookie;
+
+
+ export default setCookie;
